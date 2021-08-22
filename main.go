2793929e116af4a3a736cc08c0b677f137f82615
kaankucukx <kaankck@gmail.com>
2021-08-22 16:01:01 +0300

    Printing stuff

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	name := "Kağan"
 	age := 99
 
+	// Printf formats according to a format specifier and writes to standard output.
+	// It returns the number of bytes written and any write error encountered.
+
 	// Printinh things
 	fmt.Print("hello, ")
 	fmt.Print("boom! \n")
@@ -21,6 +24,16 @@ func main() {
 	fmt.Printf("age: %d, name: %q \n", age, name)
 
 	//Logging type of the var
-	fmt.Printf("type of age is %T \n",  age)
+	fmt.Printf("type of age is %T \n", age)
 
+	//Param logging
+	fmt.Printf("log the given %f variable \n", 999.99)
+	fmt.Printf("log the given %0.1f variable \n", 999.99)
+	fmt.Printf("log the given %0.11f variable \n", 999.99)
+
+	//fmt.Sprintf() Save formatted strings
+	var test = fmt.Sprintf("bla bla %v", name)
+	println("saved string is", test)
+
+	//https://pkg.go.dev/fmt
 }
