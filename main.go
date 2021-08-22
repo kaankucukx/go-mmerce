233eeb77029ea8039ac99931fc6824502451ae21
kaankucukx <kaankck@gmail.com>
2021-08-22 15:46:29 +0300

    Formatted log formats, see how execution context counts. The order of print order is expected

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	name := "Kağan"
+	age := 99
+
 	// Printinh things
 	fmt.Print("hello, ")
 	fmt.Print("boom! \n")
 	fmt.Print("new line! \n")
+
+	println("höllööö")
+	println("höllööö 2")
+
+	// Formated Strings
+	// See how the console output order is :)
+
+	println("formatted sTrings")
+	fmt.Printf("age: %v, name: %v", age, name)
 
 }
