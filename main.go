8f41f7586fa2f492aba92d85e34064feaf9f5fad
kaankucukx <kaankck@gmail.com>
2021-08-22 15:38:29 +0300

    new line on printing

--- main.go
+++ main.go
@@ -3,20 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// strings
-	var nameOne string = "testify"
-	var nameTwo = "testQify"
-	var nameThree string
-
-	fmt.Println(nameOne, nameTwo, nameThree)
-
-	nameFour := "kagan"
-
-	fmt.Println(nameFour)
-
-
-	var num int8 = 122
-
-	fmt.Println(num)
+	// Printinh things
+	fmt.Print("hello, ")
+	fmt.Print("boom! \n")
+	fmt.Print("new line! \n")
 
 }
