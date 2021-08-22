60e62c9c4c9f6bf1126163866f320e096860955f
kaankucukx <kaankck@gmail.com>
2021-08-22 15:31:42 +0300

    Go princiaples variables vs

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// strings
+	var nameOne string = "testify"
+	var nameTwo = "testQify"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "kagan"
+
+	fmt.Println(nameFour)
+
+
+	var num int8 = 122
+
+	fmt.Println(num)
+
+}
