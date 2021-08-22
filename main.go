73a7925ef679f69878c49b1f76b5f2cfd41b4b7a
kaankucukx <kaankck@gmail.com>
2021-08-22 15:53:06 +0300

    logging type of var

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 	// See how the console output order is :)
 
 	println("formatted sTrings")
-	fmt.Printf("age: %v, name: %v", age, name)
+	fmt.Printf("age: %d, name: %q \n", age, name)
+
+	//Logging type of the var
+	fmt.Printf("type of age is %T \n",  age)
 
 }
